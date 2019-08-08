ea3ed49de161ea238770ceec7dd3fcd054a5f73d
jeffrey <jeff.uzodinma@venturegardengroup.com>
2019-08-08 22:58:04 +0100

    added create all tables and drop all tables option on the serve.py file.

--- serve.py
+++ serve.py
@@ -1,9 +1,8 @@
 
 import os 
+import argparse
 
-
-from applib.main import app 
-from applib.model import db_session 
+from applib.main import app, db 
 from applib.lib.helper import get_config, set_db_uri
 
 
@@ -12,23 +11,39 @@ from applib.lib.helper import get_config, set_db_uri
 
 def main(): 
     
-    # >>> deprecated 
-    # folder = 'table'    
+    cmd = get_commands() 
 
-    # tables = get_config('tables', 'order')
-    
-    # with db_session() as db:
-    #     for _file in tables:
-    #         _f = os.path.join(folder, '%s.sql'%_file)
+    if cmd.create_tl:
+        db.create_all()
+        print("all missing tables have been recreated from scratch")
+        return 
 
-    #         with open(_f, 'r') as fl:
-    #             db.query(fl.read())
 
-    app.config.update(get_config('server'))
-    
-    # add the wsgi layer here be running the production code 
+    if cmd.drop_tl:
+        db.drop_all()
+        print("all tables droped from the database")
+        return 
+
+    # main app starts here 
+    app.config.update(get_config('server'))    
     app.run()
 
+
+
+def get_commands():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('-c', "--create_tl", action='store_true', 
+                        help='pass this argument to create missing tables',
+                        default=False)
+
+    parser.add_argument('-d', "--drop_tl", action='store_true', 
+                        help='pass this argument to drop all existing tables',
+                        default=False)
+
+
+    return parser.parse_args()
+ 
 
 
 # +-------------------------+-------------------------+
